77ec62a15b63c7e05e3b42294949c73fe90e73ff
houaohui <3179286437@qq.com>
2025-08-10 13:59:58 +0800

    add watchface app demo
    Signed-off-by: houaohui <3179286437@qq.com>

---
+++ app-wff-demo2/Objects/txt_viewer.d
@@ -0,0 +1,10 @@
+./objects/txt_viewer.o: txtViewer\TXT_Viewer.c txtViewer\TXT_Viewer.h \
+  C:\Keil_v5\MDK\ARM\ARMCLANG\Bin\..\include\stdbool.h \
+  C:\Keil_v5\MDK\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil_v5\MDK\ARM\ARMCLANG\Bin\..\include\wchar.h \
+  ..\app-txtReader\typedefs.h \
+  C:\Keil_v5\MDK\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\MDK\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  C:\Keil_v5\MDK\ARM\ARMCLANG\Bin\..\include\stdarg.h \
+  ..\app-txtReader\u8g2_draw_utf8.h ..\app-txtReader\typedefs.h \
+  txtViewer\txtView_task.h
